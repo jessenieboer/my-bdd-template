521e51497458b2924315efd6268b0c5569771402
Jesse Nieboer <jessenieboer@protonmail.com>
2025-04-01 11:11:50 -0500

    exp(): Remove my specific file paths from .project.el

--- .project.el
+++ .project.el
@@ -1,10 +1,10 @@
-`(:project-tags ("my_bdd_project_template")
-		:roam-files ("~/kingdom/projects/solo-projects/professional-projects/repo-templates/my-bdd-template/bdd/stories.org")
+`(:project-tags ("my_project_name")
+		:roam-files ("/path/to/this/dir/project/bdd/stories.org")
 		:roam-templates (("s" "Raw capture" plain
 				  "${title}"
-				  :target (file "~/kingdom/projects/solo-projects/professional-projects/my-bdd-template/bdd/captures.org")
+				  :target (file "/path/to/this/dir/bdd/captures.org")
 				  :empty-lines 1
 				  :immediate-finish)
 				 ("e" "Story" entry
-				  (file "~/kingdom/projects/solo-projects/professional-projects/repo-templates/my-bdd-template/bdd/story-template.org")
-				  :target (file "~/kingdom/projects/solo-projects/professional-projects/repo-templates/my-bdd-template/bdd/stories.org"))))
+				  (file "/path/to/this/dir/bdd/story-template.org")
+				  :target (file "/path/to/this/dir/bdd/stories.org"))))
