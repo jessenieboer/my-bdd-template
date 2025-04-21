59d24709b6f213cfea88b1df2f1ca3aa65a7d275
Jesse Nieboer <jessenieboer@protonmail.com>
2025-04-21 09:30:47 -0500

    exp(): Refine instructions and workflow

--- .project.el
+++ .project.el
@@ -1,8 +1,8 @@
 `(:project-tags ("my_project_name")
 		:roam-files ("/path/to/this/dir/project/bdd/stories.org")
-		:roam-templates (("s" "Raw capture" plain
+		:roam-templates (("s" "Idea" plain
 				  "${title}"
-				  :target (file "/path/to/this/dir/bdd/captures.org")
+				  :target (file "/path/to/this/dir/bdd/ideas.txt")
 				  :empty-lines 1
 				  :immediate-finish)
 				 ("e" "Story" entry
