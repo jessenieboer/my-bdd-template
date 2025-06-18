69de719c66d26a2fddf1cb1271e22888ece31571
Jesse Nieboer <jessenieboer@protonmail.com>
2025-06-18 10:59:35 -0500

    src(): Set my-repo-template as no-push remote in gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,3 +7,5 @@ git config commit.template .gitmessage
 git config core.autocrlf false
 git config core.fileMode false
 git config core.safecrlf true
+git remote add my-repo-template git@github.com:jessenieboer/my-repo-template.git
+git remote set-url --push my-repo-template no_push
